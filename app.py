9b477b09af4f8442531316584cb620c9501e97fa
Miraç <fatalbloow@gmail.com>
2025-05-25 21:52:42 +0300

    port degisti

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def index():
     return render_template("index.html", result=result, assets=assets.keys())
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
